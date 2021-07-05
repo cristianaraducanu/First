fc345863dc9585814e292e600f876d476b107cf6
Cristiana Raducanu <cristinicardcn@gmail.com>
2021-07-05 19:51:54 +0300

    Created feature branch and modified main class

--- src/com/cris/Main.java
+++ src/com/cris/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Congrats, master branch looks fine");
         System.out.println("Now you're on develop");
+        System.out.println("And this is the final branch, feature");
     }
 }
