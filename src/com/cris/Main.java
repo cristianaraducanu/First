14420bb0ff2d752d08d925b8b9e35bea6208a9e1
Cristiana Raducanu <cristinicardcn@gmail.com>
2021-07-05 19:50:30 +0300

    Created develop branch and modified main class

--- src/com/cris/Main.java
+++ src/com/cris/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Congrats, master branch looks fine");
+        System.out.println("Now you're on develop");
     }
 }
